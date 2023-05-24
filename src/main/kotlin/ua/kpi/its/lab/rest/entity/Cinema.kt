56ff80a618f26245cbc3d0f01f8d725cc7352b38
Oleksii <>
2023-05-24 22:23:40 +0300

    lab3

--- src/main/kotlin/ua/kpi/its/lab/rest/entity/Cinema.kt
+++ src/main/kotlin/ua/kpi/its/lab/rest/entity/Cinema.kt
@@ -1,6 +1,15 @@
+package ua.kpi.its.lab.rest.entity
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.*
 import java.time.LocalDate
-
+@Entity
+@Table(name = "cinema")
 data class Cinema(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
     val name: String,
     val address: String,
     val launchDate: LocalDate,
@@ -8,6 +17,7 @@ data class Cinema(
     val numOfScreens: Int,
     val soundTechnology: String,
     val is3DAvailable: Boolean,
+    @OneToMany(mappedBy = "movies", cascade = [CascadeType.ALL], orphanRemoval = true)
     val movies: List<Movie>
 ) : Comparable<Cinema> {
     override fun compareTo(cinema: Cinema): Int {
